236710d87b16f015fd3b639ef9ec819adb3b7325
Karoife <joserodolfo_coco@hotmail.com>
2023-02-16 06:20:07 -0600

    Primer commit, agregando una primer version de la clase Jugador

---
+++ src/code/jugador.py
@@ -0,0 +1,27 @@
+# ----------------------------------------------
+# Author:   Jose Rodolfo //  Fernando Vargas
+# Date:     16/02/2023
+# Version:  1.0
+# ----------------------------------------------
+# Clase Jugador 
+# Utilizada para guardar los puntajes
+# ----------------------------------------------
+
+class Jugador:
+
+    def __init__(self, id, nombre):
+        self.puntaje = 0
+        self.id = id
+        self.nombre = nombre
+    
+    def getNombre(self):
+        return self.nombre
+
+    def getId(self):
+        return self.id
+
+    def getPuntaje(self):
+        return self.puntaje
+
+    def setPuntaje(self, puntajeNuevo):
+        self.puntaje = puntajeNuevo
