3af0fead2a30eed0ce219738dc68cbcdc1395338
Karoife <joserodolfo_coco@hotmail.com>
2023-02-18 17:19:14 -0600

    haciendo pull del tablero y cambiandolo

--- src/code/tablero.py
+++ src/code/tablero.py
@@ -13,19 +13,24 @@ class Tablero:
         self.printTerm = list()
         self.resultados = list()
         self.llenarResultados()
+        self.llenarPrintTerm()
+
     def getResultados(self):
         return self.resultados
+
     def gerPrinTerm(self):
         return self.printTerm
+
     def llenarResultados(self):
         for i in range(49): # se crea resultados para las 49 casillas del tablero
             if i % 5 == 0 and i != 0: # cada cinco espacios se coloca un monstruo
-                self.resultados[i] = "m"
+                self.resultados[i].append("m")
             else:
-                self.resultados[i] = random.randint(2,1500) # se genera 40 numeros aleatoreos para las casillas que no tienen monstruo
+                self.resultados[i].append(random.randint(2,1500)) # se genera 40 numeros aleatoreos para las casillas que no tienen monstruo
+
     def llenarPrintTerm(self):
         for i in range(49): 
             if i % 5 == 0 and i != 0:
-                self.resultados[i] = "m"
+                self.resultados[i].append("m")
             else:
-                self.printTerm[i] = random.randint(1,self.resultados[i])
+                self.printTerm[i].append(random.randint(1,self.resultados[i]))
